d578f4b9caddb4524c685e63028015632ccedfc0
miciek <miciek@gmail.com>
2017-03-15 23:05:26 +0100

    Add initial version of RemoteServerInterpreter and MultiplayerServer (WIP)

---
+++ src/main/scala/com/michalplachta/cats/free/MultiplayerServer.scala
@@ -0,0 +1,71 @@
+package com.michalplachta.cats.free
+
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+import com.michalplachta.cats.free.PrisonersDilemma._
+import com.typesafe.config.ConfigFactory
+
+object MultiplayerServer extends App {
+  final case class PrisonerPlayer(name: String, actorRef: ActorRef, decision: Option[Decision])
+
+  class Server extends Actor with ActorLogging {
+    var playerA: Option[PrisonerPlayer] = None
+    var playerB: Option[PrisonerPlayer] = None
+
+    def receive: Receive = {
+      case playerName: String ⇒
+        endGameIfAllPlayersDefined()
+        val newPlayer = PrisonerPlayer(playerName, sender, None)
+        if (playerA.isEmpty) {
+          playerA = Some(newPlayer)
+        } else {
+          playerB = Some(newPlayer)
+          announcePlayers()
+        }
+        log.info(s"Registered $playerName")
+
+      case (playerName: String, opponentName: String, decisionStr: String) ⇒
+        def playerWithDecision(player: Option[PrisonerPlayer], opponent: Option[PrisonerPlayer], playerDecision: Decision) =
+          if (player.exists(_.name == playerName)) player.map(_.copy(decision = Some(playerDecision))) else player
+
+        val playerDecision = if (decisionStr == "Guilty") Guilty else Silence
+        playerA = playerWithDecision(playerA, playerB, playerDecision)
+        playerB = playerWithDecision(playerB, playerA, playerDecision)
+
+        endGameIfAllDecisionsReceived()
+        log.info(s"Received decision of $playerName against $opponentName: $decisionStr ($playerDecision)")
+    }
+
+    private def announcePlayers(): Unit = {
+      def announcePlayer(maybePlayer: Option[PrisonerPlayer], maybeOpponent: Option[PrisonerPlayer]) = for {
+        player ← maybePlayer
+        opponent ← maybeOpponent
+      } yield player.actorRef ! opponent.name
+      announcePlayer(playerA, playerB)
+      announcePlayer(playerB, playerA)
+    }
+
+    private def endGameIfAllDecisionsReceived(): Unit =
+      if (playerA.exists(_.decision.isDefined) && playerB.exists(_.decision.isDefined)) endGame()
+
+    private def endGameIfAllPlayersDefined(): Unit =
+      if (playerA.isDefined && playerB.isDefined) endGame()
+
+    private def endGame(): Unit = {
+      resolveGame()
+      playerA = None
+      playerB = None
+    }
+
+    private def resolveGame(): Unit = {
+      def decision(player: Option[PrisonerPlayer]) = player.flatMap(_.decision).getOrElse(Silence)
+      def sendVerdict(player: Option[PrisonerPlayer], opponent: Option[PrisonerPlayer]) =
+        player.foreach { _.actorRef ! verdict(decision(player), decision(opponent)).years }
+      sendVerdict(playerA, playerB)
+      sendVerdict(playerB, playerA)
+      log.info(s"Resolved game between $playerA and $playerB")
+    }
+  }
+
+  val system = ActorSystem("prisonersDilemma", ConfigFactory.load("server"))
+  val server = system.actorOf(Props[Server], "server")
+}
---
+++ src/main/scala/com/michalplachta/cats/free/RemoteServerInterpreter.scala
@@ -0,0 +1,38 @@
+package com.michalplachta.cats.free
+
+import akka.actor.{ Actor, ActorSelection, ActorSystem }
+import akka.pattern.ask
+import cats.~>
+import com.michalplachta.cats.free.PrisonersDilemma.{ Prisoner, Verdict }
+import com.michalplachta.cats.free.ServerDSL.{ GetOpponentFor, SendDecision, Server }
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+object RemoteServerInterpreter extends (Server ~> Future) {
+  val system = ActorSystem("gameClient", ConfigFactory.load("client"))
+  val server: ActorSelection = system.actorSelection("akka.tcp://prisonersDilemma@127.0.0.1:2552/user/server")
+
+  /*
+   * Note: the protocol between client and remote server uses Strings only.
+   */
+  def apply[A](i: Server[A]): Future[A] = i match {
+    case GetOpponentFor(prisoner) ⇒
+      server
+        .ask(prisoner.name)(60.seconds)
+        .mapTo[String]
+        .map(Prisoner)
+
+    case SendDecision(prisoner, otherPrisoner, decision) ⇒
+      // FIXME: this doesn't work properly with the current MultiplayerServer implementation
+      server
+        .ask((prisoner.name, otherPrisoner.name, decision.toString))(60.seconds)
+        .mapTo[Int]
+        .map(Verdict)
+  }
+
+  def terminate(): Unit = system.terminate()
+}
+
