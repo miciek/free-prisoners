31e64c100944a744163de6aa7b80ed48a4de09f9
miciek <miciek@gmail.com>
2017-11-17 07:29:35 +0100

    Run GameServer actor at the start of new MultiplayerServer app

--- src/main/scala/com/michalplachta/freeprisoners/apps/MultiplayerServer.scala
+++ src/main/scala/com/michalplachta/freeprisoners/apps/MultiplayerServer.scala
@@ -1,10 +1,14 @@
 package com.michalplachta.freeprisoners.apps
 
 import akka.actor.{ActorSystem, Props}
-import com.michalplachta.freeprisoners.actors.MatchmakingServerActor
+import com.michalplachta.freeprisoners.actors.{
+  GameServerActor,
+  MatchmakingServerActor
+}
 
-object MatchmakingServer extends App {
+object MultiplayerServer extends App {
   private val system = ActorSystem("prisonersDilemma")
   system.actorOf(Props[MatchmakingServerActor], "matchmakingServer")
+  system.actorOf(Props[GameServerActor], "gameServer")
   println("Server is running...")
 }
