05f74378f40fe9d4b1b6188e94bb7ed3723dae38
miciek <miciek@gmail.com>
2017-12-11 20:53:22 +0100

    Refactor random function in BotInterpreter

--- src/main/scala/com/michalplachta/freeprisoners/free/interpreters/BotInterpreter.scala
+++ src/main/scala/com/michalplachta/freeprisoners/free/interpreters/BotInterpreter.scala
@@ -20,24 +20,23 @@ class BotInterpreter extends (Opponent ~> IO) {
   val names = Array("Wall-E", "R2-D2", "Megatron", "T-800")
   val strategies = Array(Strategy(_ => Guilty), Strategy(_ => Silence))
   val r = new Random()
-
   var bots = Map.empty[Prisoner, Strategy]
 
   /*_*/
   def apply[A](i: Opponent[A]): IO[A] = i match {
     case MeetOpponent(_) =>
       IO {
-        val n = r.nextInt(names.length)
-        val prisoner = Prisoner(names(n))
-
-        val s = r.nextInt(strategies.length)
-        bots += (prisoner -> strategies(s))
+        val prisoner = Prisoner(random(names))
+        bots += (prisoner -> random(strategies))
         Some(prisoner)
       }
 
     case GetOpponentDecision(player, opponent) =>
-      IO {
-        bots.get(opponent).map(_.f(player)).getOrElse(Silence)
-      }
+      IO { bots.get(opponent).map(_.f(player)).getOrElse(Silence) }
+  }
+
+  private def random[T](array: Array[T]): T = {
+    val i = r.nextInt(array.length)
+    array(i)
   }
 }
